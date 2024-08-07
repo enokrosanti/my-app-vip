db806d0a9fecce3481449bbe8dca591d9c532e19
enokrosanti <enokrosanti54@gmail.com>
2024-08-07 14:34:10 +0700

    if false will be redirected to the login page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const isLogin = true;
+  const isLogin = false;
   if (isLogin) {
     return NextResponse.next();
   } else {
