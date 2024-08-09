e993b7a34d19f1faf1462b59e70c653c29a6dc98
enokrosanti <enokrosanti54@gmail.com>
2024-08-09 11:23:47 +0700

    add signOut

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,11 +1,16 @@
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const {data} = useSession();
   return (
     <div className={styles.navbar}>
       <div className="big">Navbar</div>
+      {data ? (
+      <button onClick={() => signOut()}>Sign Out</button>
+    ): (
       <button onClick={() => signIn()}>Sign In</button>
+    )}
     </div>
   );
 };
